47097f247f9ef81d908e7c9c8421b6d9c060d603
keringo <>
2025-05-26 08:02:41 +0300

    tabs

--- cmake/PythonSetup.cmake
+++ cmake/PythonSetup.cmake
@@ -3,11 +3,11 @@
 #-----------------------------------------------------------------------------
 
 if(WIN32)
-    execute_process(COMMAND ${CHOCO} install python -y --no-progress)
+	execute_process(COMMAND ${CHOCO} install python -y --no-progress)
 elseif(APPLE)
-    execute_process(COMMAND ${BREW} install python)
+	execute_process(COMMAND ${BREW} install python)
 else()
-    execute_process(COMMAND sudo ${APT_GET} install -y python3-dev python3-venv)
+	execute_process(COMMAND sudo ${APT_GET} install -y python3-dev python3-venv)
 endif()
 
 find_package(Python3 3.11 REQUIRED COMPONENTS Interpreter Development)
@@ -19,11 +19,11 @@ file(MAKE_DIRECTORY ${PYTHON_VENV_DIR})
 execute_process(COMMAND ${Python3_EXECUTABLE} -m venv "${PYTHON_VENV_DIR}")
 
 if(WIN32)
-    set(VENV_PIP_EXECUTABLE "${PYTHON_VENV_DIR}/Scripts/pip.exe")
-    set(VENV_SITE_PACKAGES "${PYTHON_VENV_DIR}/Lib/site-packages")
+	set(VENV_PIP_EXECUTABLE "${PYTHON_VENV_DIR}/Scripts/pip.exe")
+	set(VENV_SITE_PACKAGES "${PYTHON_VENV_DIR}/Lib/site-packages")
 else()
-    set(VENV_PIP_EXECUTABLE "${PYTHON_VENV_DIR}/bin/pip")
-    set(VENV_SITE_PACKAGES "${PYTHON_VENV_DIR}/lib/python${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}/site-packages")
+	set(VENV_PIP_EXECUTABLE "${PYTHON_VENV_DIR}/bin/pip")
+	set(VENV_SITE_PACKAGES "${PYTHON_VENV_DIR}/lib/python${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}/site-packages")
 endif()
 
 #-----------------------------------------------------------------------------
@@ -31,9 +31,9 @@ endif()
 #-----------------------------------------------------------------------------
 
 execute_process(
-    COMMAND ${VENV_PIP_EXECUTABLE} install --upgrade
-    translators
-    python-iso639
+	COMMAND ${VENV_PIP_EXECUTABLE} install --upgrade
+	translators
+	python-iso639
 )
 
 #-----------------------------------------------------------------------------
--- cmake/QtSetup.cmake
+++ cmake/QtSetup.cmake
@@ -5,33 +5,33 @@
 set(QT_COMPONENTS Core Gui Widgets Sql Qml Network)
 
 if(WIN32)
-    execute_process(COMMAND ${CHOCO} install qt6-base-dev -y --version 6.4.2 --no-progress --params="/InstallationPath C:/Qt")
-    set(QT_PATHS 
-        "C:/Qt/6.4.2/msvc2019_64/lib/cmake/Qt6"
-        "C:/Qt/6.4.2/msvc2019_64"
-        "C:/Qt/6.4.2/msvc2022_64/lib/cmake/Qt6"
-        "C:/Qt/6.4.2/msvc2022_64"
-        "C:/Qt/6.4.2/mingw_64/lib/cmake/Qt6"
-        "C:/Qt/6.4.2/mingw_64"
-        "$ENV{QT_DIR}"
-        "$ENV{QTDIR}"
-    )
-    find_path(QT_DIR 
-        NAMES Qt6Config.cmake
-        PATHS ${QT_PATHS}
-        REQUIRED
-    )
+	execute_process(COMMAND ${CHOCO} install qt6-base-dev -y --version 6.4.2 --no-progress --params="/InstallationPath C:/Qt")
+	set(QT_PATHS 
+		"C:/Qt/6.4.2/msvc2019_64/lib/cmake/Qt6"
+		"C:/Qt/6.4.2/msvc2019_64"
+		"C:/Qt/6.4.2/msvc2022_64/lib/cmake/Qt6"
+		"C:/Qt/6.4.2/msvc2022_64"
+		"C:/Qt/6.4.2/mingw_64/lib/cmake/Qt6"
+		"C:/Qt/6.4.2/mingw_64"
+		"$ENV{QT_DIR}"
+		"$ENV{QTDIR}"
+	)
+	find_path(QT_DIR 
+		NAMES Qt6Config.cmake
+		PATHS ${QT_PATHS}
+		REQUIRED
+	)
 elseif(APPLE)
-    execute_process(COMMAND ${BREW} install qt@6)
-    execute_process(
-        COMMAND ${BREW} --prefix qt@6
-        OUTPUT_VARIABLE QT_PREFIX
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    set(QT_DIR "${QT_PREFIX}/lib/cmake/Qt6")
+	execute_process(COMMAND ${BREW} install qt@6)
+	execute_process(
+		COMMAND ${BREW} --prefix qt@6
+		OUTPUT_VARIABLE QT_PREFIX
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	set(QT_DIR "${QT_PREFIX}/lib/cmake/Qt6")
 else()
-    execute_process(COMMAND sudo ${APT_GET} install -y libgl1-mesa-dev qt6-base-dev qt6-declarative-dev)
-    set(QT_DIR "/usr/lib/x86_64-linux-gnu/cmake/Qt6")
+	execute_process(COMMAND sudo ${APT_GET} install -y libgl1-mesa-dev qt6-base-dev qt6-declarative-dev)
+	set(QT_DIR "/usr/lib/x86_64-linux-gnu/cmake/Qt6")
 endif()
 
 list(APPEND CMAKE_PREFIX_PATH ${QT_DIR})
