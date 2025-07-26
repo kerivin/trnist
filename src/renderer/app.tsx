7726bd8d03d5a87f501ebcf354e0b721a8e91518
kerivin <kerivin.zero@gmail.com>
2025-07-27 01:00:50 +0300

    slate minimal

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,3 +1,12 @@
+import { Editor } from './components/Editor';
+
 export default function App() {
-  return(<h1>TestTestttt</h1>);
+  return(
+    <div style={{ padding: '20px' }}>
+      <h1>Slate</h1>
+      <div style={{ border: '1px solid #ccc', padding: '10px', minHeight: '200px' }}>
+        <Editor />
+      </div>
+    </div>
+  );
 }
---
+++ src/renderer/components/Editor.tsx
@@ -0,0 +1,53 @@
+// import { useState, useMemo } from 'react';
+// import { createEditor, Descendant } from 'slate';
+// import { Slate, Editable, withReact } from 'slate-react';
+
+// const initialValue: Descendant[] = [
+//   {
+//     type: 'paragraph',
+//     children: [{ text: 'A line of text in a paragraph.' }],
+//   },
+// ];
+
+// export const Editor = () => {
+//   const [editor] = useState(() => withReact(createEditor()));
+  
+//   return (
+//     <Slate editor={editor} initialValue={initialValue}>
+//       <Editable />
+//     </Slate>
+//   );
+// };
+
+
+import React, { useState } from 'react';
+import { createEditor } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
+
+import { BaseEditor, Descendant } from 'slate';
+import { ReactEditor } from 'slate-react';
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] };
+type CustomText = { text: string };
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+};
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]
+
+export const Editor = () => {
+    const [editor] = useState(() => withReact(createEditor()));
+    return (<Slate editor={editor} initialValue={initialValue}>
+        <Editable />
+    </Slate>);
+};
